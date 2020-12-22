6be9e82c10bcee6e3b18cd38db53bbb83649d6b0
Nilesh Pant <nileshpant6969@gmail.com>
2020-12-22 13:04:09 +0530

    bug fix for netlify

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,22 +1,28 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
+import Spinner from "../components/ui/Spinner";
 
 function Character(props) {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const id = props.match.params.id;
 
   const fetchCharacter = async () => {
+    setIsLoading(true);
     const result = await Axios(
       `https://www.breakingbadapi.com/api/characters/${id}`
     );
     setItems(result.data[0]);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchCharacter();
   }, []);
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="character__details">
       <img src={items.img} alt="" height="500px" width="500px" />
       <div>
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -24,13 +24,10 @@ function HomeScreen() {
     const slice = result.data.slice(offset, offset + characterPerPage);
 
     // console.log(result.data)
-
+    console.log(slice);
     setItems(slice);
     setIsLoading(false);
   };
-
-  console.log(currentPage);
-  console.log(setCharacterPerPage);
 
   useEffect(() => {
     fetchItems();
