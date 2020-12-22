9ccf01657e18f5f1f803884c3527d9b8d13b25cc
Nilesh Pant <nileshpant6969@gmail.com>
2020-12-22 14:06:31 +0530

    bug fix

--- src/components/Character.js
+++ src/components/Character.js
@@ -25,23 +25,6 @@ function Character(props) {
   ) : (
     <div className="character__details">
       <img src={items.img} alt="" height="500px" width="500px" />
-      <table style="width:100%">
-        <tr>
-          <th>Name</th>
-          <th>Lastname</th>
-          <th>Age</th>
-        </tr>
-        <tr>
-          <td>Jill</td>
-          <td>Smith</td>
-          <td>50</td>
-        </tr>
-        <tr>
-          <td>Eve</td>
-          <td>Jackson</td>
-          <td>94</td>
-        </tr>
-      </table>
       <div className="details">
         <h1> Name: {items.name}</h1>
         <h1>Actor: {items.portrayed}</h1>
